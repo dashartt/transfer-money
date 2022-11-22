6ece77a7e244a02b1fc8411e23b41b77c20a15ef
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-11-22 17:32:13 -0300

    feat: :sparkles: config routes and chakra provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,19 @@
 import './index.css';
 
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
