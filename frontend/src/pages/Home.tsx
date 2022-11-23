e1f193e56144b9e4c529a9facb26a6470f8cb952
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-11-23 16:28:34 -0300

    feat: :sparkles: add home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,31 @@
-import { Box } from '@chakra-ui/react';
+import { Box, SimpleGrid, Stack } from '@chakra-ui/react';
+
+import AccountSession from '../components/account-session/AccountSession';
+import Balance from '../components/balance/Balance';
+import History from '../components/history/History';
+import Transfer from '../components/transfer/Transfer';
 
 export default function Home() {
-  return <Box>Home page</Box>;
+  return (
+    <Box minH="100vh" bg="main.green" padding={{ base: 'auto', lg: '10em 20em' }}>
+      <SimpleGrid columns={1} spacing={5}>
+        <Stack
+          direction={{ base: 'column', sm: 'row' }}
+          bg="whitesmoke"
+          padding="2em"
+          border="1px solid black"
+          justifyContent="space-between"
+        >
+          <AccountSession />
+          <Balance />
+        </Stack>
+        <Box bg="whitesmoke" padding="2em" border="1px solid black">
+          <Transfer />
+        </Box>
+        <Box bg="whitesmoke" padding="2em" border="1px solid black">
+          <History />
+        </Box>
+      </SimpleGrid>
+    </Box>
+  );
 }
