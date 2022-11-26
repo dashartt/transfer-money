b9966aea166e615438cab07bdc3a56b1bba118fe
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-11-26 04:15:06 -0300

    feat: :sparkles: add User service

---
+++ backend/src/services/UserService.ts
@@ -0,0 +1,37 @@
+import { userSchema } from "../config/zodSchemas";
+import UserModel, { User } from "../models/UserModel";
+
+class UserService {
+  private userModel;
+
+  constructor() {
+    this.userModel = new UserModel();
+  }
+
+  async register(data: User) {
+    const hasError = this.validateData(data);
+    if (hasError) return hasError;
+
+    await this.userModel.register(data);
+  }
+
+  async login(data: User) {
+    const hasError = this.validateData(data);
+
+    if (hasError) return hasError;
+
+    await this.userModel.login(data);
+  }
+
+  validateData(data: User) {
+    const parsed = userSchema.safeParse(data);
+
+    if (!parsed.success) {
+      const errors = parsed.error.issues.map((error) => error.message);
+      const errorMessage = { message: `400|${errors}` };
+      return errorMessage;
+    }
+  }
+}
+
+export default UserService;
