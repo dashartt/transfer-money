a8ee48a78052a690ac639aa0074ad0e42fde1af6
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-11-26 20:42:30 -0300

    feat: :sparkles: add Transaction routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,11 +1,27 @@
 import { NextFunction, Request, Response, Router } from "express";
 import UserController from "../controllers/UserController";
+import TransactionController from "../controllers/TransactionController";
 
 const router = Router();
 const userController = new UserController();
+const transactionController = new TransactionController();
 
 router.post("/auth", (req: Request, res: Response, next: NextFunction) =>
   userController.auth(req, res, next)
+);
+
+router.post(
+  "/transactions/transfer",
+  (req: Request, res: Response, next: NextFunction) => {
+    transactionController.registerTransfer(req, res, next);
+  }
+);
+
+router.get(
+  "/transactions/history/:id",
+  (req: Request, res: Response, next: NextFunction) => {
+    transactionController.getTransactionHistory(req, res, next);
+  }
 );
 
 export default router;
