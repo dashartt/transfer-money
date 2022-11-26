90b8ab5d266f4a98c5d220f7e5ea29861ffd84c5
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-11-26 20:30:35 -0300

    feat: :sparkles: add Transaction controller

---
+++ backend/src/controllers/TransactionController.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import TransactionService from "../services/TransactionService";
+
+class TransactionController {
+  private transactionService;
+
+  constructor() {
+    this.transactionService = new TransactionService();
+  }
+
+  async registerTransfer(req: Request, res: Response, next: NextFunction) {
+    const hasError = await this.transactionService.registerTransfer(req.body);
+
+    if (hasError) return next(hasError);
+
+    return res.status(200).end();
+  }
+
+  async getTransactionHistory(req: Request, res: Response, next: NextFunction) {
+    const transactionHistory =
+      await this.transactionService.getTransactionHistory(
+        Number(req.params.id)
+      );
+
+    return res.status(200).json({ data: transactionHistory });
+  }
+}
+
+export default TransactionController;
