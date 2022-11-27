8ec6fe9e64d6b57482bb5fb22239d63add11f156
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-11-27 18:13:01 -0300

    fix: :adhesive_bandage: frontend url

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,7 +9,7 @@ const errorHandler = new ErrorHandler().catchErrors;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3001/",
+    origin: "http://localhost:5173",
   })
 );
 app.use(routes);
