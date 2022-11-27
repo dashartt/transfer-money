be6bb9b3c47015abfd78015ebb50bbabe3882267
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-11-27 20:36:57 -0300

    feat: :sparkles: backend requests

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+type AuthData = {
+  username: string | undefined;
+  password: string | undefined;
+};
+
+const request = axios.create({
+  baseURL: 'http://localhost:3001/',
+});
+
+export const requestAuth = async (reqData: AuthData, typeAuth: string) => {
+  return request
+    .post(`/auth?option=${typeAuth}`, { ...reqData })
+    .then(({ data }) => data.message)
+    .catch((error) => error.response.data.errors);
+};
