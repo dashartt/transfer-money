2a9c4c0d06a0fb3d4f112bc6bca87ba76b4e2e96
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-11-27 20:41:07 -0300

    feat: :wrench: add Toast setup

---
+++ frontend/src/utils/toastConfig.tsx
@@ -0,0 +1,12 @@
+import { UseToastOptions } from '@chakra-ui/react';
+
+const toastConfig: UseToastOptions = {
+  position: 'top',
+  title: 'Information',
+  status: 'info',
+  duration: 10000,
+  variant: 'top-accent',
+  isClosable: true,
+};
+
+export default toastConfig;
