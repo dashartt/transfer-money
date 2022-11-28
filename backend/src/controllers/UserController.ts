c7c936a1df5ef3518cbfe46cf77f6471223ca4e6
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-11-28 18:15:04 -0300

    refactor: :recycle: add username and balance in login request response

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from "express";
+import AccountService from "../services/AccountService";
 import UserService from "../services/UserService";
+import { UserResponseOutput } from "../types/UserTypes";
 
 class UserController {
   private service;
@@ -10,22 +12,37 @@ class UserController {
 
   async auth(req: Request, res: Response, next: NextFunction) {
     const authOption = req.query?.option as string;
-    const hasError = this.service.validateData(req.body);
+    const responseOutput = this.service.validateData(req.body);
 
-    if (hasError) return next(hasError);
+    if (responseOutput.fail) return next(responseOutput.fail);
 
     if (authOption === "register") {
       await this.register(req, res);
     } else {
-      await this.login(req, res);
+      await this.login(req, res, next);
     }
   }
 
-  async login(req: Request, res: Response) {
-    const token = await this.service.login(req.body);
-    return res
-      .status(200)
-      .json({ token, message: "User authenticated successfully" });
+  async login(req: Request, res: Response, next: NextFunction) {
+    const userResponseOutput = await this.service.login(req.body);
+
+    const { token, username } = userResponseOutput.success
+      ?.data as UserResponseOutput;
+
+    const accountResponseOutput = await new AccountService().getAccountDetails(
+      username
+    );
+
+    if (accountResponseOutput.fail) return next(accountResponseOutput.fail);
+    const { balance } = accountResponseOutput.success
+      ?.data as UserResponseOutput;
+
+    return res.status(200).json({
+      token,
+      username,
+      balance,
+      message: "User authenticated successfully",
+    });
   }
 
   async register(req: Request, res: Response) {
