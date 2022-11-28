6f2f05acdd4b5e084020c1787328d39e9ac91c7f
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-11-27 21:31:18 -0300

    feat: :sparkles: add logout in home page

--- frontend/src/components/account-session/AccountSession.tsx
+++ frontend/src/components/account-session/AccountSession.tsx
@@ -1,6 +1,14 @@
 import { Button, Heading, VStack } from '@chakra-ui/react';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 export default function AccountSession() {
+  const navigate = useNavigate();
+
+  const logOut = () => {
+    localStorage.removeItem('token');
+    navigate('/auth');
+  };
+
   return (
     <VStack>
       <Heading alignSelf={{ base: 'flex-start', lg: 'baseline' }} as="h1">
@@ -12,6 +20,7 @@ export default function AccountSession() {
         alignSelf="flex-start"
         color="white"
         bgColor="main.green"
+        onClick={logOut}
       >
         Logout
       </Button>
