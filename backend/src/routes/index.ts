44bb3f07be0d283e4895a81f51ca9833dfd0b7d2
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-11-28 09:42:38 -0300

    refactor: :recycle: uses middleware to check if the transfer is valid

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,10 +2,12 @@ import { NextFunction, Request, Response, Router } from "express";
 import UserController from "../controllers/UserController";
 import TransactionController from "../controllers/TransactionController";
 import checkAuth from "../middlewares/CheckAuth";
+import AccountController from "../controllers/AccountController";
 
 const router = Router();
 const userController = new UserController();
 const transactionController = new TransactionController();
+const accountController = new AccountController();
 
 router.post("/auth", (req: Request, res: Response, next: NextFunction) =>
   userController.auth(req, res, next)
@@ -14,6 +16,15 @@ router.post("/auth", (req: Request, res: Response, next: NextFunction) =>
 router.post(
   "/transactions/transfer",
   checkAuth,
+  (req: Request, res: Response, next: NextFunction) => {
+    accountController.getAccountIdsToTransfer(req, res, next);
+  },
+  (req: Request, res: Response, next: NextFunction) => {
+    accountController.validateBalance(req, res, next);
+  },
+  (req: Request, res: Response, next: NextFunction) => {
+    accountController.makeTransfer(req, res, next);
+  },
   (req: Request, res: Response, next: NextFunction) => {
     transactionController.registerTransfer(req, res, next);
   }
