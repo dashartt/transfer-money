2bbfb77ebe1b17b152546fa215e7c01ebf36abd1
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-11-28 18:25:54 -0300

    feat: :sparkles: shows the user's current account balance

--- frontend/src/components/balance/Balance.tsx
+++ frontend/src/components/balance/Balance.tsx
@@ -1,14 +1,26 @@
 import { Box, Heading, Text, VStack } from '@chakra-ui/react';
+import { useContext, useEffect } from 'react';
+
+import { BalanceContext, BalanceContextValue } from '../../contexts/BalanceProvider';
+import { AuthedUserDTO } from '../../types/RequestData';
 
 export default function Balance() {
+  const { balance, setBalance } = useContext(BalanceContext) as BalanceContextValue;
+
+  useEffect(() => {
+    const userData = JSON.parse(localStorage.getItem('user') || '') as AuthedUserDTO;
+
+    setBalance(userData.balance);
+  }, []);
+
   return (
     <Box>
       <VStack justifyContent="space-between">
         <Heading alignSelf={{ base: 'flex-start', lg: 'baseline' }} as="h1">
           Balance
         </Heading>
-        <Text alignSelf="flex-start" fontWeight="bold">
-          R$0.0
+        <Text fontSize="1.4rem" alignSelf="flex-start" fontWeight="bold">
+          {`R$${balance}`}
         </Text>
       </VStack>
     </Box>
