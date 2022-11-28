4675352ef8a1a1d17db444dde8a7a71780f29722
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-11-28 18:22:37 -0300

    feat: :sparkles: add transfer endpoint

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,17 +1,21 @@
 import axios from 'axios';
 
-type AuthData = {
-  username: string | undefined;
-  password: string | undefined;
-};
+import { AuthRequestDTO, TransferRequestDTO } from '../types/RequestData';
 
 const request = axios.create({
   baseURL: 'http://localhost:3001/',
 });
 
-export const requestAuth = async (reqData: AuthData, typeAuth: string) => {
+export const requestAuth = async (reqData: AuthRequestDTO, typeAuth: string) => {
   return request
     .post(`/auth?option=${typeAuth}`, { ...reqData })
+    .then(({ data }) => data)
+    .catch((error) => error.response.data);
+};
+
+export const requestTransfer = async (reqData: TransferRequestDTO) => {
+  return request
+    .post('/transactions/transfer', { reqData })
     .then(({ data }) => data)
     .catch((error) => error.response.data);
 };
