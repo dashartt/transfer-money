0ff8c9692e7fbf2678fdbf7aa930b71b46bc266d
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-11-30 18:23:38 -0300

    fix: :adhesive_bandage: change frontend port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,7 +9,7 @@ const errorHandler = new ErrorHandler().catchErrors;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:3000",
   })
 );
 app.use(routes);
