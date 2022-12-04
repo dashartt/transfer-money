8c45a14aa859506cd02f01b520d6331f50931446
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-12-04 02:58:21 -0300

    feat: :clown_face: add login failed user response mock

--- backend/tests/mocks/responseMocks.ts
+++ backend/tests/mocks/responseMocks.ts
@@ -8,10 +8,15 @@ export const registeredUserResponse = {
   message: "Account created successfully. Login with your account to proceed",
 } as ResponseOutput;
 
-export const authenticatedUserResponse = {
+export const loginSucessUserResponse = {
   token: "fake_token",
   username: "dashartx",
   balance: 0,
   status: 200,
   message: "User authenticated successfully",
+};
+
+export const loginFailedUserResponse = {
+  status: 400,
+  message: "Invalid username or password",
 };
