e9a7ad073aab540ccb074ca5d140f2a6df85f931
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-12-04 09:36:21 -0300

    feat: :label: add new types

--- backend/src/custom.d.ts
+++ backend/src/custom.d.ts
@@ -1,4 +1,6 @@
 import { JwtPayload } from "jsonwebtoken";
+import { AccountDetails } from "./types/AccountTypes";
+import { LoginSuccessNext } from "./types/ServerTypes";
 import {
   AccountForTransfer,
   AccountsIdsForTransfer,
@@ -8,6 +10,9 @@ import { AuthedUserResponse, UserToken } from "./types/UserTypes";
 declare global {
   namespace Express {
     interface Request {
+      loginSuccessData?: LoginSuccessNext;
+      accountDetails?: AccountDetails;
+
       tokenData?: UserToken;
       accountsForTransfer?: AccountForTransfer;
       accountsIdsForTransfer?: AccountsIdsForTransfer;
