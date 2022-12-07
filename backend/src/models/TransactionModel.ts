9bc2279ff5e3e82b78643a077c5c1952e4f2d096
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-12-07 04:33:39 -0300

    fix: :adhesive_bandage: remove duplicate data

--- backend/src/models/TransactionModel.ts
+++ backend/src/models/TransactionModel.ts
@@ -23,7 +23,7 @@ class TransactionModel {
 
   async getTransactionHistory(accountId: number) {
     const transactionHistory = (await this.prisma.$queryRaw`    
-      SELECT us1.username as 'creditedAccount', us2.username as 'debitedAccount', tr.value as 'inCome', (acc.balance + tr.value) as 'outCome', tr.createdAt as 'datetime'
+      SELECT DISTINCT us1.username as 'creditedAccount', us2.username as 'debitedAccount', tr.value as 'inCome', tr.createdAt as 'datetime'
       FROM Transaction as tr
       INNER JOIN Account as acc
       ON acc.id = tr.creditedAccountId OR acc.id = tr.debitedAccountId
